dd97df19ec46327c4559e29978e7fb83c668fdd3
Hasaan591gb <ahmad591gb@gmail.com>
2023-03-28 03:39:05 -0700

    update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,22 +1,13 @@
 #include "main.h"
 
-/**
- * print_buffer - prints contet of buffer
- * @buffer: buffer to print
- * @buff_ind: counter
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+#include "main.h"
 
-	*buff_ind = 0;
-}
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - replica of standard library printf function
- * @format: input string
- * Return: number of chars printed
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
@@ -61,4 +52,17 @@ int _printf(const char *format, ...)
 	va_end(list);
 
 	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
